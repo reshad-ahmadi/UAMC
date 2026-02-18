08f7d08429a88eb7fb7d0f368b9b57b6b5bdf924
morshed-murad <morshedmorad2002@gmail.com>
2026-02-18 16:07:19 +0430

    feat: update it react router

--- Assoiation/src/App.tsx
+++ Assoiation/src/App.tsx
@@ -1,18 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import Companies from './pages/Companies';
 import AdminPanel from './pages/AdminPanel';
+import Navbar from './components/Nav';
+import Footer from './components/Footer';
+
+function Layout() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/members" element={<Companies />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<AboutUs />} />
+          <Route path="members" element={<Companies />} />
+          {/* We can add a contact route later if needed, but for now it's on Home */}
+        </Route>
         <Route path="/admin" element={<AdminPanel />} />
-        {/* We can add a contact route later if needed, but for now it's on Home */}
       </Routes>
     </Router>
   );
